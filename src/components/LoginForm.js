cd841942c4bfe470f5cdb0e35af64f66695e436f
GaganBassi <gaganbassi12@gmail.com>
2024-02-07 18:53:57 -0500

    Validations Concept

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -9,13 +9,19 @@ const LoginForm = () => {
 
     const email1=useRef(null);
     const password1=useRef(null);
+   // const message=useRef('Gagan');
+   const [output, setOutput]=useState('');
 
     const toggleSignIn=()=>{
         setIsSignInForm(!isSignInForm);
     }
     const validate=()=>{
-      console.log(email1.current.value, password1.current.value); //Way to use the Ref Variable and fetch the current value from 
+      //console.log(email1.current.value, password1.current.value); //Way to use the Ref Variable and fetch the current value from 
       //from input box
+
+      const message=checkValidateForm(email1.current.value, password1.current.value);
+      console.log(message);
+      setOutput(message);
     }
   return (
     <div>
@@ -32,6 +38,7 @@ const LoginForm = () => {
         <input type='password' ref={password1}
         placeholder='Password' className='p-4 my-4 w-full bg-gray-300'/>
         <button className='p-4 my-6 bg-red-700 w-full rounded-lg' onClick={validate}>{isSignInForm? "Sign In":"Sign Up"}</button>
+        <p className='text-red-500 font-bold text-lg p-2'>{output}</p>
         <p className='py-4 cursor-pointer' onClick={toggleSignIn}>{isSignInForm? "New to Netflix? Sign up now.":"Already user? Click to Login"}</p>
       </form>
     </div>
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -4,8 +4,12 @@ export const checkValidateForm=(email,password)=>{
     const IsEmailId=/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
     const IsPasswordValid=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
-    return(IsEmailId===false && "Email Id is not valid");
-    return(IsPasswordValid===false && "Password is not valid");
+    if(IsEmailId===false){
+        return(IsEmailId===false && "Email Id is not valid");}
+
+    if(IsPasswordValid===false){
+        return ("Password is not valid")
+    }
 
     return null;// if both are true then return null.
 
