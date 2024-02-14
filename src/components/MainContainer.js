62af9d28feb2d71d2bf8ed0f91e9e33c3a58f613
GaganBassi <gaganbassi12@gmail.com>
2024-02-14 15:48:29 -0500

    SetBackgroundImage

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -6,9 +6,15 @@ import VideoBackground from './VideoBackground'
 const MainContainer = () => {
 
     const movies1=useSelector((store)=>{return store.movies?.nowPlayingMovies});
+   
+    //console.log("gen",genuineMovie);
+    
     if(movies1===null) return;//if no movies in a store then return from here.//known as early return
+    const temp=[movies1[0],movies1[1],movies1[6],movies1[4],movies1[7]];
+    const random = Math.floor(Math.random() * temp.length);//to select random value
+    console.log(random);
 
-    const mainMovie=movies1[0];//if movies are there in a store then execute from here.
+    const mainMovie=temp[random];//if movies are there in a store then execute from here.
     console.log(mainMovie);
 
     const {original_title,overview,id}=mainMovie;
